89dd6ff97e1076155f9351078eb9702f99550c07
arax89 <alexander-kessler@web.de>
2013-12-12 14:59:18 +0100

    - Türen hinzugefügt (mit Animation, Trigger usw.)

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+	public Transform Door;
+
+	bool isOpen = false;
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider player) {
+		if(player.tag == "Player") {
+			Debug.Log(player.tag);
+		
+			Door.animation.CrossFade("DoorOpen");
+
+		}
+	}
+
+	void OnTriggerExit(Collider player) {
+		if(player.tag == "Player"){
+		
+			Door.animation.CrossFade("DoorClose");
+		
+		
+		}
+	}
+}
