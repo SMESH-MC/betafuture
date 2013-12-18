5e4ba8084557c36ca89e2ad1b697bf159988ba37
arax89 <alexander-kessler@web.de>
2013-12-18 17:00:47 +0100

    Levelziel fix Nr 2 - Farben hinzugefügt

---
+++ Assets/LevelChangerFixed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangerFixed : MonoBehaviour {
+
+	public int levelauswahl = 2;
+
+ 	void OnTriggerEnter(Collider player) {
+		if (player.tag == "Player") {
+			Application.LoadLevel(levelauswahl);
+		}
+	}
+}
