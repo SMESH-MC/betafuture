c517ae78120328adb2d916a7769853a840b71802
derb <demiusend@gmail.com>
2013-12-18 14:31:43 +0100

    neues move-skript für gegner

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+	
+	public float speed = 5;
+	private Vector2 move;
+	private int i;
+	// Use this for initialization
+	void Start () {
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		walk ();
+	}
+	
+	void walk() {
+		if (i >= 0 & i < 10) {
+			move.x = -speed;
+			i++;
+		} else {
+			move.x = speed;
+			i--;
+		}
+	}
+	
+}
