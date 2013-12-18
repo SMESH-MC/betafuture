3c424726f57e884e25bac38881529542d6085703
tjakobs <tudilli@web.de>
2013-12-18 15:23:57 +0100

    Levelziel eingefügt. Testweise einen Raum tiefer um ihn erreichen zu können. Allerdings rastet der Player bei kontakt aus, sodass ich nicht testen konnte.

---
+++ Assets/Scripts/LevelChanger.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+var levelauswahl = 2;
+
+function OnControllerColliderHit(hit : ControllerColliderHit){
+	if (hit.gameObject.tag == "Finish"){
+		Application.LoadLevel(levelauswahl);
+	}
+}
