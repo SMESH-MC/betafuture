76efaf0f3b5e3821a80ad24322af9a15558a3938
tjakobs <tudilli@web.de>
2013-12-18 18:55:10 +0100

    Fix hitPlayer

--- Assets/hitPlayer.cs
+++ Assets/hitPlayer.cs
@@ -3,25 +3,28 @@ using System.Collections;
 
 public class hitPlayer : MonoBehaviour {
 
-	private health playerhealth;
+	//private health playerhealth;
 	// Use this for initialization
 	void Start () {
 	
 	}
 
-    int Awake { 
-		set { playerhealth = GameObject.FindWithTag ("Player").GetComponent<health> ();}
-	}
+    //int Awake { 
+	//	set { playerhealth = GameObject.FindWithTag ("Player").GetComponent<health> ();}
+	//}
 
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-	void OnTriggerEnter(Collider player) {
+	void OnTriggerEnter(Collider playerCollider) {
 
-		if(player.tag == "Player") {
-			playerhealth.TakeDamage();
+		if(playerCollider.tag == "Player") {
+			GameObject player = GameObject.Find("Player");
+			health playerHealth = (health) player.GetComponent(typeof(health));
+			playerHealth.TakeDamage();
+			//playerhealth.TakeDamage();
 		}
 	}
 
