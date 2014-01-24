da396bb3cd45b5c759c39d02da8a81ee075bbfaa
arax89 <alexander-kessler@web.de>
2014-01-24 13:42:20 +0100

    -shooter-mechanik angefangen (linke maustaste zum schießen)
    - enemy-skripte entfernt
    -alte player-physics und player-controller scripts entfernt

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMovement : MonoBehaviour {
+
+	private CharacterController controller;
+	private GameObject player;
+	private Vector3 dir  = Vector3.zero;
+
+
+	public float speed  = 6.0f;
+	public float gravity = 9.81f;
+	public float jumpPower = 5.0f;
+	public bool flipped = false;
+
+	void Awake () {
+		controller = this.GetComponent<CharacterController>();
+		player = this.gameObject;
+	}
+
+	void Update () {
+		move ();
+		flipSide();
+	}
+
+	void move() {
+		if(controller.isGrounded){
+			dir.x = Input.GetAxis("Horizontal") * speed;
+			if(Input.GetButtonDown("Jump")){
+				dir.y = jumpPower;
+			}
+		}
+		else {
+			dir.y = dir.y - gravity;
+			dir.x = (Input.GetAxis("Horizontal") * speed );
+		}
+
+		if(dir.x > 0){
+			flipped = false;
+		} else if (dir.x < 0) {
+			flipped = true;
+		} 
+		controller.Move(dir * Time.deltaTime);
+	}
+
+	void flipSide() {
+		if (flipped){
+			player.transform.rotation = Quaternion.Euler(0, 180, 0);
+		}
+		else{
+			player.transform.transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+	}
+}
---
+++ Assets/ShootingScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingScript : MonoBehaviour {
+
+	public Rigidbody projectile;
+	private Rigidbody clone;
+	public float speed = 20.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetButtonDown("Fire1")) {
+			clone = (Rigidbody) Instantiate(projectile, transform.position, transform.rotation);
+			clone.velocity = transform.right * speed;
+
+			Destroy(clone.gameObject, 3);
+		}
+	}
+}
