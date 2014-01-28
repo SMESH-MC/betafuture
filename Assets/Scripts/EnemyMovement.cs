3f2458b4874a3edbcba990f406927b5c46172350
d-erb <demiusend@gmail.com>
2014-01-28 22:57:18 +0100

    der Enemy erlernt das Sehen

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,6 +8,12 @@ public class EnemyMovement : MonoBehaviour {
 	private GameObject enemy;
 	private Vector3 dir  = Vector3.zero;
 
+
+	private GameObject player;
+	public float viewField = 10.0f;
+	public bool seePlayer = false;
+
+
 	public float moveDirection = 1;
 	public float speed  = 6.0f;
 	public float gravity = 9.81f;
@@ -16,15 +22,51 @@ public class EnemyMovement : MonoBehaviour {
 	public bool flipped = false;
 
 	void Awake () {
+		player = GameObject.Find ("Player");
 		distance = moveLength;
 		controller = this.GetComponent<CharacterController>();
 		enemy = this.gameObject;
 	}
 
 	void Update () {
+		seePlayer = playerIsinView ();
 		move ();
 		flipSide();
 	}
+
+
+
+	bool playerIsinView() {
+			float posPlayerX = player.transform.position.x;
+			float posPlayerY = player.transform.position.y;
+			
+			float posX = enemy.transform.position.x;
+			float posY = enemy.transform.position.y;
+			
+			float isInView = 100;
+			
+			float yDiv = Mathf.Abs ( posY - posPlayerY );
+			// sind auf selber Ebene
+			if (yDiv <= 6.0) {
+				if (!flipped) {
+					posX += viewField;
+					isInView = posPlayerX - posX;
+				} else {	//hier noch ein problem
+					posX -= viewField;
+					isInView = posPlayerX + posX;
+				}
+
+
+				if ( isInView <= 0 ) {
+					return true;
+				}	
+			}
+		return false;
+	}
+	                    
+
+
+
 
 	void move() {
 		float dirXOld = transform.position.x;
