d865bc34439b94df535a6a0c6bd232d8a71b9ee4
ckoenig86 <christian.koenig86@web.de>
2014-02-02 14:36:11 +0100

    aufzug geändert. trotzdem sagt die physik immer noch "fuck you"

---
+++ Assets/Scripts/elevator.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var amplitude: float = 5;
+var speed: float = 0.2;
+var direction: Vector3 = Vector3.up; 
+private var p0: Vector3;
+ 
+function Start()
+{
+	p0 = transform.position;
+	while (true)
+	{
+		var dir = transform.TransformDirection(direction);
+		transform.position = p0+amplitude*dir*Mathf.Sin(6.28*speed*Time.time);
+		yield;
+	}
+}
