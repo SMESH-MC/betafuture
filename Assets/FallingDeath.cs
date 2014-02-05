bdfc4cf8006492da319d6cd328dc31aa201c8561
arax89 <alexander-kessler@web.de>
2014-02-05 22:01:32 +0100

    FallingDeath implementiert

---
+++ Assets/FallingDeath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingDeath : MonoBehaviour {
+
+	public float deadline;
+	private GameObject player;
+	private float playerY;
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerY = player.transform.position.y;
+		if(playerY < deadline) {
+			Object.Destroy(player);
+		}
+	}
+}
