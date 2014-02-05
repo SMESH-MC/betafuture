b2ac4ad7a159ec8569d45aad8c73fb01006f1582
smeshmc <smeshmc+sourceforge@gmail.com>
2014-02-05 20:44:45 +0100

    menueauswahl lvl2

--- Assets/Scripts/levelmenu.js
+++ Assets/Scripts/levelmenu.js
@@ -15,8 +15,7 @@ GUI.skin = mySkin;
 	
 	if (GUI.Button (Rect(Screen.width/2-150,Screen.height/2,300,30),"Level 1")){
 	
-	//Application.LoadLevel("level1");
-	print ("Auskommentieren #2");
+	Application.LoadLevel("level2");
 	
 	}
 	
