db7c0d6ecba07d380d7805731db579a41213c7cb
arax89 <alexander-kessler@web.de>
2014-02-05 22:17:17 +0100

    HUD on Dead fix

--- Assets/Scripts/camera_lock.cs
+++ Assets/Scripts/camera_lock.cs
@@ -5,6 +5,11 @@ public class camera_lock : MonoBehaviour
 {
 	public Vector3 offset;			// The offset at which the Health Bar follows the player.
 	public GUIText deadscreen;
+
+	private GUITexture hpBar;
+	private GUITexture shBar;
+	private GUIText hpText;
+
 	private Transform player;		// Reference to the player.
 
 	private int onScreenX;
@@ -13,6 +18,9 @@ public class camera_lock : MonoBehaviour
 	
 	void Awake ()
 	{
+		hpBar = GameObject.Find("HealthBar").GetComponent<GUITexture>();
+		shBar = GameObject.Find("ShieldBar").GetComponent<GUITexture>();
+		hpText = GameObject.Find("HealthBarText").GetComponent<GUIText>();
 		loadedLevel = Application.loadedLevel;
 		deadscreen = GameObject.Find("deadscreen").GetComponent<GUIText>();
 		onScreenX = Screen.width/2;
@@ -26,11 +34,17 @@ public class camera_lock : MonoBehaviour
 	{
 		if(player != null) {
 			deadscreen.enabled = false;
+			hpText.enabled = true;
+			hpBar.enabled = true;
+			shBar.enabled = true;
 		// Set the position to the player's position with the offset.
 		transform.position = player.position + offset;
 		}
 		else {
 			deadscreen.enabled = true;
+			hpText.enabled = false;
+			hpBar.enabled = false;
+			shBar.enabled = false;
 			if(Input.GetButtonDown("Jump")) {
 				Application.LoadLevel(loadedLevel);
 			}
